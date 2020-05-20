52b1b310e4a576cba2f908c66958d02a16799f2a
shohail <shohail@cloudthing.com>
2020-05-20 13:24:09 +0530

    Updates

---
+++ precache-manifest.508efb3c6e78909cb5cadbdad8e63507.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faed5a114f65c02b475d96dd33cef15f",
+    "url": "/react-recoil/index.html"
+  },
+  {
+    "revision": "85c73410b3d4cce68803",
+    "url": "/react-recoil/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "9bb23013cc8fe6dc80b2",
+    "url": "/react-recoil/static/js/2.76174863.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-recoil/static/js/2.76174863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85c73410b3d4cce68803",
+    "url": "/react-recoil/static/js/main.141966d8.chunk.js"
+  },
+  {
+    "revision": "008ec7154d3d28a1602b",
+    "url": "/react-recoil/static/js/runtime-main.eefd777f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-recoil/static/media/logo.5d5d9eef.svg"
+  }
+]);
